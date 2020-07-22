50a3d49cfe8f804711b306aeedd0b1618ce18cad
AlexeyZelenko <zelenkooleksii75@gmail.com>
2020-07-22 12:36:44 +0300

    2

---
+++ src/components/v-catalog-item.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+      <p>Item 1</p>
+      <p>Price</p>
+      <button>Add to cart</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "v-catalog-item"
+    }
+</script>
+
+<style>
+
+</style>
