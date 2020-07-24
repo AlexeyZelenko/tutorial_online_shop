bd96b2dd393d20e7a2f86ecf544f76989252818e
AlexeyZelenko <zelenkooleksii75@gmail.com>
2020-07-24 11:01:16 +0300

    Подключил firebase

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@
 
 <script>
     import vMainWrapper from './components/v-main-wrapper'
+    import 'firebase/firestore'
 
     export default {
         name: 'App',
--- src/main.js
+++ src/main.js
@@ -2,9 +2,30 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './vuex/store'
 import router from './router/router'
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/database'
+import 'firebase/auth'
+import { firestorePlugin } from 'vuefire'
 // import './assets/styles/styles.scss'
 
 Vue.config.productionTip = false
+
+
+
+export const db = firebase.initializeApp({
+    apiKey: "AIzaSyBMnm2m-5DVxNkJYghP8Jo41V9m3fvf0Tc",
+    authDomain: "online-shop-34af2.firebaseapp.com",
+    databaseURL: "https://online-shop-34af2.firebaseio.com",
+    projectId: "online-shop-34af2",
+    storageBucket: "online-shop-34af2.appspot.com",
+    messagingSenderId: "206555332095",
+    appId: "1:206555332095:web:9eead21cb681b196f73bca",
+    measurementId: "G-QSTP059LSZ"
+}).firestore()
+
+Vue.use(firestorePlugin)
+
 
 new Vue({
   render: h => h(App),
