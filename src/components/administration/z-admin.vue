083da6eb09317531b82b49c7c5d71622d50fea83
AlexeyZelenko <zelenkooleksii75@gmail.com>
2020-07-29 14:31:51 +0300

    выбор языка, админ панэль

--- src/components/administration/z-admin.vue
+++ src/components/administration/z-admin.vue
@@ -1,48 +1,22 @@
 <template>
-	<div class="">
+	<div >
 		<router-link :to="{name: 'catalog'}">
-			<div class="v-catalog__link_to_admin">{{'Back to catalog' | localize }}</div>
+			<div
+					style="height: 45px"
+					class="v-catalog__link_to_admin">
+				{{'Back to catalog' | localize }}
+			</div>
 		</router-link>
 
 		<z-add-new-product/>
 
-		<div>
-			<h5
-				style="color: darkcyan"
-			>
-				Добавить карточку с товаром
-			</h5>
 		</div>
-
-		<form
-				@submit="addLocation(name, article, available, category, image, name, price)">
-			<div class="form-group">
-        <textarea
-					placeholder="Название"
-					v-model="name">
-        </textarea>
-				<textarea
-						placeholder="Добавить артикль товара"
-						v-model="article">
-          </textarea>
-			</div>
-			<div class="modal-footer">
-				<button
-						class="btn btn-info"
-						type="submit">
-					Создать
-				</button>
-			</div>
-		</form>
-		</div>
-
 </template>
 
 <script>
     import 'material-design-icons-iconfont'
     import 'materialize-css/dist/js/materialize.min'
     import zAddNewProduct from './z-add-new-product'
-    import {db} from '@/main.js'
 
     export default {
         name: "zAdmin",
@@ -51,27 +25,11 @@
 				},
         data() {
             return {
-                openMessage: true,
                 message: [],
                 location2: [],
-                name: '',
-                article: '',
-                available: true,
-                category: '',
-                image: '',
-                price: 0,
             }
         },
         computed: {},
-        methods: {
-            addLocation(article, available, category, image, name, price) {      // <-- новый метод
-                const createdAt = new Date()
-                db.collection('products').add({article, available, createdAt, category, image, name, price})
-            },
-            deleteLocation(id) {   // <-- новый метод
-                db.collection('message').doc(id).delete()
-            }
-        }
     }
 </script>
 
