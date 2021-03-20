deab791959ba4e98782cb8c1dd507b7cf84bbb53
AlexeyZelenko <zelenkooleksii75@gmail.com>
2021-03-20 12:28:52 +0200

    Версия online-shop-timofey 0.0.5 Инфо о товаре - подробнее

--- src/pages/catalog/z-product-information.vue
+++ src/pages/catalog/z-product-information.vue
@@ -38,118 +38,136 @@
 <!--          Все про товар-->
           <v-tab-item>
             <v-card flat class="my-4">
-              <v-carousel
-                  :carousel_data="product.arrayImages"
-              />
 
-              <p>{{"Price" | localize}}: {{product.price}} грн</p>
-              <p>{{"Descriptions" | localize}}:</p>
-              <p v-html="product.description"></p>
-              <p v-if="product.clothingManufacturer !== '' ">{{"Manufacturer country" | localize}} :
-                {{product.clothingManufacturer}}</p>
-              <p v-if="product.BrandName !== '' ">{{"Brand name" | localize}}: {{product.BrandName}}</p>
+              <v-card-text>
+                <v-row align="center">
+                  <v-col
+                      class="display-3"
+                      cols="12"
+                      md="6"
+                  >
+                    <v-carousel
+                        :carousel_data="product.arrayImages"
+                    />
+                  </v-col>
+                  <v-col
+                      cols="12"
+                      md="6"
+                  >
+                    <div>
+                      <p>{{"Price" | localize}}: {{product.price}} грн</p>
+                      <p>{{"Descriptions" | localize}}:</p>
+                      <p v-html="product.description"></p>
+                      <p v-if="product.clothingManufacturer !== '' ">{{"Manufacturer country" | localize}} :
+                        {{product.clothingManufacturer}}</p>
+                      <p v-if="product.BrandName !== '' ">{{"Brand name" | localize}}: {{product.BrandName}}</p>
 
-              <!--Выбрать размер-->
-              <!--		<button-->
-              <!--				@click.stop="dialog = true"-->
-              <!--				class="v-catalog_item_add_cart_btn btn"-->
-              <!--		>-->
-              <!--			Выбрать размер-->
-              <!--		</button>-->
-              <!--		<p>{{'Европа : '}}{{this.select.state}}</p>-->
-              <!--		<p>{{'Международная система : '}}{{this.select.abbr}}</p>-->
+                      <!--Выбрать размер-->
+                      <!--		<button-->
+                      <!--				@click.stop="dialog = true"-->
+                      <!--				class="v-catalog_item_add_cart_btn btn"-->
+                      <!--		>-->
+                      <!--			Выбрать размер-->
+                      <!--		</button>-->
+                      <!--		<p>{{'Европа : '}}{{this.select.state}}</p>-->
+                      <!--		<p>{{'Международная система : '}}{{this.select.abbr}}</p>-->
 
-              <!--		<p>{{"Clothing size" | localize}}: {{product.clothingSize}}</p>-->
+                      <!--		<p>{{"Clothing size" | localize}}: {{product.clothingSize}}</p>-->
 
 
-              <p>Артикль товара: {{product.article}}</p>
-              <div class="text-center">
-                <v-chip
-                    class="ma-2"
-                    style="background-color: goldenrod; color:white"
-                    text-color="white"
-                    v-if="product.newClothes"
-                >
-                  НОВИНКА!
-                  <v-icon right style="color: white">mdi-star</v-icon>
-                </v-chip>
-                <!--					СКИДКА-->
-                <v-chip
-                    class="v_catalog_item_new" style="background-color: #da207d; color:white"
-                    text-color="white"
-                    v-if="product.promotionalPrice"
-                >
-                  {{"Promotional Price" | localize}} !!!
-                </v-chip>
-              </div>
-              <!--		<div data-app >-->
-              <!--			<template >-->
-              <!--				<v-row justify="center">-->
+                      <p>Артикль товара: {{product.article}}</p>
+                      <div class="text-center">
+                        <v-chip
+                            class="ma-2"
+                            style="background-color: goldenrod; color:white"
+                            text-color="white"
+                            v-if="product.newClothes"
+                        >
+                          НОВИНКА!
+                          <v-icon right style="color: white">mdi-star</v-icon>
+                        </v-chip>
+                        <!--					СКИДКА-->
+                        <v-chip
+                            class="v_catalog_item_new" style="background-color: #da207d; color:white"
+                            text-color="white"
+                            v-if="product.promotionalPrice"
+                        >
+                          {{"Promotional Price" | localize}} !!!
+                        </v-chip>
+                      </div>
+                      <!--		<div data-app >-->
+                      <!--			<template >-->
+                      <!--				<v-row justify="center">-->
 
-              <!--					<v-dialog-->
-              <!--							v-model="dialog"-->
-              <!--							max-width="290"-->
-              <!--					>-->
-              <!--						<v-card>-->
-              <!--							<v-card-title class="headline">Выберите размер</v-card-title>-->
+                      <!--					<v-dialog-->
+                      <!--							v-model="dialog"-->
+                      <!--							max-width="290"-->
+                      <!--					>-->
+                      <!--						<v-card>-->
+                      <!--							<v-card-title class="headline">Выберите размер</v-card-title>-->
 
-              <!--&lt;!&ndash;							<v-card-text>&ndash;&gt;-->
-              <!--&lt;!&ndash;								Let Google help apps determine location. This means sending anonymous location data to Google, even when no apps are running.&ndash;&gt;-->
-              <!--&lt;!&ndash;							</v-card-text>&ndash;&gt;-->
+                      <!--&lt;!&ndash;							<v-card-text>&ndash;&gt;-->
+                      <!--&lt;!&ndash;								Let Google help apps determine location. This means sending anonymous location data to Google, even when no apps are running.&ndash;&gt;-->
+                      <!--&lt;!&ndash;							</v-card-text>&ndash;&gt;-->
 
-              <!--							<template>-->
-              <!--								<v-container fluid>-->
-              <!--									<v-row align="center">-->
+                      <!--							<template>-->
+                      <!--								<v-container fluid>-->
+                      <!--									<v-row align="center">-->
 
-              <!--										<v-col cols="12">-->
-              <!--											<v-select-->
-              <!--													v-model="select"-->
-              <!--													:hint="`${select.state}, ${select.abbr}`"-->
-              <!--													:items="items"-->
-              <!--													item-text="state"-->
-              <!--													item-value="abbr"-->
-              <!--													label="Выберите размер одежды"-->
-              <!--													persistent-hint-->
-              <!--													return-object-->
-              <!--													single-line-->
-              <!--											></v-select>-->
-              <!--										</v-col>-->
-              <!--									</v-row>-->
-              <!--								</v-container>-->
-              <!--							</template>-->
+                      <!--										<v-col cols="12">-->
+                      <!--											<v-select-->
+                      <!--													v-model="select"-->
+                      <!--													:hint="`${select.state}, ${select.abbr}`"-->
+                      <!--													:items="items"-->
+                      <!--													item-text="state"-->
+                      <!--													item-value="abbr"-->
+                      <!--													label="Выберите размер одежды"-->
+                      <!--													persistent-hint-->
+                      <!--													return-object-->
+                      <!--													single-line-->
+                      <!--											></v-select>-->
+                      <!--										</v-col>-->
+                      <!--									</v-row>-->
+                      <!--								</v-container>-->
+                      <!--							</template>-->
 
-              <!--							<v-card-actions>-->
-              <!--								<v-spacer></v-spacer>-->
+                      <!--							<v-card-actions>-->
+                      <!--								<v-spacer></v-spacer>-->
 
-              <!--								<v-btn-->
-              <!--										color="green darken-1"-->
-              <!--										text-->
-              <!--										@click.stop="dialog = false"-->
-              <!--								>-->
-              <!--									Закрыть-->
-              <!--								</v-btn>-->
+                      <!--								<v-btn-->
+                      <!--										color="green darken-1"-->
+                      <!--										text-->
+                      <!--										@click.stop="dialog = false"-->
+                      <!--								>-->
+                      <!--									Закрыть-->
+                      <!--								</v-btn>-->
 
-              <!--								<v-btn-->
-              <!--										style="margin-top: 20px"-->
-              <!--										text-->
-              <!--										@click.stop="dialog = false"-->
-              <!--								>-->
-              <!--									Ок-->
-              <!--								</v-btn>-->
-              <!--							</v-card-actions>-->
-              <!--						</v-card>-->
-              <!--					</v-dialog>-->
-              <!--				</v-row>-->
-              <!--			</template>-->
-              <!--		</div>-->
+                      <!--								<v-btn-->
+                      <!--										style="margin-top: 20px"-->
+                      <!--										text-->
+                      <!--										@click.stop="dialog = false"-->
+                      <!--								>-->
+                      <!--									Ок-->
+                      <!--								</v-btn>-->
+                      <!--							</v-card-actions>-->
+                      <!--						</v-card>-->
+                      <!--					</v-dialog>-->
+                      <!--				</v-row>-->
+                      <!--			</template>-->
+                      <!--		</div>-->
 
-              <button
-                  @click="addToCart"
-                  class="v-catalog_item_add_cart_btn btn"
-                  style="margin-top: 20px"
-              >
-                {{'Add to cart' | localize}}
-              </button>
+                      <button
+                          @click="addToCart"
+                          class="v-catalog_item_add_cart_btn btn"
+                          style="margin-top: 20px"
+                      >
+                        {{'Add to cart' | localize}}
+                      </button>
+                    </div>
+                  </v-col>
+                </v-row>
+              </v-card-text>
+
             </v-card>
           </v-tab-item>
 
@@ -231,7 +249,6 @@
             }
           ],
           tabs: null,
-          text: 'gdghfgs',
             // select: { state: '36-38', abbr: 'S' },
             // items: [
             //     { state: '36-38', abbr: 'S' },
