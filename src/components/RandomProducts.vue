b2b29dd06b4997ca584c4ea585cce76380bc249d
AlexeyZelenko <zelenkooleksii75@gmail.com>
2021-03-29 20:13:27 +0300

    Версия online-shop-timofey 0.1.5 Уникальные в рандоме

--- src/components/RandomProducts.vue
+++ src/components/RandomProducts.vue
@@ -1,44 +1,31 @@
 <template>
   <v-card class="my-2">
     <h5 class="py-2">Вам может понравиться:</h5>
-    <v-tabs
-        color="deep-purple accent-4"
-        right
-    >
-      <v-tab>назад</v-tab>
-      <v-tab>вперед</v-tab>
-
-      <v-tab-item
-          v-for="n in 2"
-          :key="n"
-      >
-        <v-container fluid>
-          <v-row>
-            <v-col
-                v-for="item in RANDOM_PRODUCTS"
-                :key="item.article"
-                cols="12"
-                md="2"
+    <v-container fluid>
+      <v-row>
+        <v-col
+            v-for="item in RANDOM_PRODUCTS"
+            :key="item.article"
+            cols="12"
+            md="2"
+        >
+          <p style="color: darkcyan">{{item.name}}</p>
+          <div class="text-center">
+            <v-btn
+                @click="productClick(item.article)"
+                color="primary"
+                text
             >
-              <p style="color: darkcyan">{{item.name}}</p>
-              <div class="text-center">
-                <v-btn
-                    @click="productClick(item.article)"
-                    color="primary"
-                    text
-                >
-                  Подробнее >>
-                </v-btn>
-              </div>
-              <v-img
-                  :src="item.arrayImages[0]"
-                  aspect-ratio="1"
-              ></v-img>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-tab-item>
-    </v-tabs>
+              Подробнее >>
+            </v-btn>
+          </div>
+          <v-img
+              :src="item.arrayImages[0]"
+              aspect-ratio="1"
+          ></v-img>
+        </v-col>
+      </v-row>
+    </v-container>
   </v-card>
 </template>
 
@@ -63,12 +50,11 @@ export default {
       'GET_PRODUCTS'
     ]),
     RANDOM_PRODUCTS() {
-      const a = this.GET_PRODUCTS
-      const b = a.sort(() => Math.random() - 0.5)
-
-      return b.filter((obj, idx, arr) => (
+      const a = this.GET_PRODUCTS.filter((obj, idx, arr) => (
           arr.findIndex((o) => o.name === obj.name) === idx
       ))
+
+      return a.sort(() => Math.random() - 0.5)
     }
   }
 }
