a714d5b73c4450bbd4b20bc1f3c26df0c8d81702
AlexeyZelenko <zelenkooleksii75@gmail.com>
2021-05-01 12:14:29 +0300

    Версия online-shop-timofey 0.2.2 Защита роутов

--- src/router/router.js
+++ src/router/router.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import firebase from 'firebase/app'
 
 
 Vue.use(Router)
@@ -29,7 +30,7 @@ let router = new Router({
             path: '/admin',
             name: 'admin',
             component: () => import('@/pages/administration/z-admin'),
-            meta: { requiresAuth: true },
+            meta: { auth: true },
             props: true
         },
         {
@@ -48,19 +49,21 @@ let router = new Router({
             path: '/information-order',
             name: 'information-order',
             component: () => import('@/pages/administration/z-information-order'),
-            props: true
+            props: true,
+            meta: { auth: true },
         },
         {
             path: '/order-description',
             name: 'order-description',
             component: () => import('@/pages/administration/z-order-description'),
-            props: true
+            props: true,
+            meta: { auth: true },
         },
         {
             path: '/cabinetUser',
             name: 'cabinetUser',
             component: () => import('@/pages/catalog/cabinetUser'),
-            props: true
+            props: true,
         },
         {
           path: '/reviews',
@@ -69,6 +72,29 @@ let router = new Router({
           props: true
         },
     ]
+})
+
+router.beforeEach((to, from, next) => {
+  const currentUser = firebase.auth().currentUser
+  const requireAuth = to.matched.some(record => record.meta.auth)
+
+  if (currentUser != null) {
+    const email = currentUser.email;
+    const uid = currentUser.uid;
+
+    console.log(email)
+    console.log(uid)
+
+    const adminEntrance = ['q77OchyqmyavLxjnlMiQlcxLefw2', "wH7hb4Zdh9Xqt2RZRMAnJa3Nko23", "hng6vLzPtTYo5xgiuYyjYpOnijB2","HInmvosDanObSDnC2csXiV3iR0A2", "s0LUjzE0wPZ4LsYgr81A3YmyaQA3"].includes(uid)
+    console.log(adminEntrance)
+
+    if (requireAuth && !adminEntrance) {
+      next('/')
+    } else {
+      next()
+    }
+  }
+
 })
 
 export default router
