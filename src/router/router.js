1a0009169a91ad451c61eadd1c71d643763b5e7f
AlexeyZelenko <zelenkooleksii75@gmail.com>
2021-05-01 12:45:18 +0300

    Версия online-shop-timofey 0.2.2 Защита роутов2

--- src/router/router.js
+++ src/router/router.js
@@ -6,77 +6,84 @@ import firebase from 'firebase/app'
 Vue.use(Router)
 
 let router = new Router({
-    mode: 'history',
-    routes: [
-        {
-            path: '/',
-            name: 'catalog',
-            meta: {layout: 'main'},
-            component: () => import('@/pages/catalog/v-catalog'),
-        },
-        {
-            path: '/cart',
-            name: 'cart',
-            component: () => import('@/pages/cart/v-cart'),
-            props: true
-        },
-        {
-            path: '/product',
-            name: 'product',
-            component: () => import('@/pages/catalog/z-product-information'),
-            props: true
-        },
-        {
-            path: '/admin',
-            name: 'admin',
-            component: () => import('@/pages/administration/z-admin'),
-            meta: { auth: true },
-            props: true
-        },
-        {
-            path: '/login',
-            name: 'login',
-            component: () => import('@/pages/login'),
-            props: true
-        },
-        {
-            path: '/registration',
-            name: 'registration',
-            component: () => import('@/pages/registration'),
-            props: true
-        },
-        {
-            path: '/information-order',
-            name: 'information-order',
-            component: () => import('@/pages/administration/z-information-order'),
-            props: true,
-            meta: { auth: true },
-        },
-        {
-            path: '/order-description',
-            name: 'order-description',
-            component: () => import('@/pages/administration/z-order-description'),
-            props: true,
-            meta: { auth: true },
-        },
-        {
-            path: '/cabinetUser',
-            name: 'cabinetUser',
-            component: () => import('@/pages/catalog/cabinetUser'),
-            props: true,
-        },
-        {
-          path: '/reviews',
-          name: 'reviews',
-          component: () => import('@/pages/Reviews'),
-          props: true
-        },
-    ]
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'catalog',
+      meta: {layout: 'main', auth: false},
+      component: () => import('@/pages/catalog/v-catalog'),
+    },
+    {
+      path: '/cart',
+      name: 'cart',
+      component: () => import('@/pages/cart/v-cart'),
+      props: true,
+      meta: { auth: false }
+    },
+    {
+      path: '/product',
+      name: 'product',
+      component: () => import('@/pages/catalog/z-product-information'),
+      props: true,
+      meta: { auth: false }
+    },
+    {
+      path: '/admin',
+      name: 'admin',
+      component: () => import('@/pages/administration/z-admin'),
+      meta: { auth: true },
+      props: true,
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('@/pages/login'),
+      props: true,
+      meta: { auth: false }
+    },
+    {
+      path: '/registration',
+      name: 'registration',
+      component: () => import('@/pages/registration'),
+      props: true,
+      meta: { auth: false }
+    },
+    {
+      path: '/information-order',
+      name: 'information-order',
+      component: () => import('@/pages/administration/z-information-order'),
+      props: true,
+      meta: { auth: true },
+    },
+    {
+      path: '/order-description',
+      name: 'order-description',
+      component: () => import('@/pages/administration/z-order-description'),
+      props: true,
+      meta: { auth: true },
+    },
+    {
+      path: '/cabinetUser',
+      name: 'cabinetUser',
+      component: () => import('@/pages/catalog/cabinetUser'),
+      props: true,
+      meta: { auth: false }
+    },
+    {
+      path: '/reviews',
+      name: 'reviews',
+      component: () => import('@/pages/Reviews'),
+      props: true
+    },
+  ]
 })
 
 router.beforeEach((to, from, next) => {
   const currentUser = firebase.auth().currentUser
   const requireAuth = to.matched.some(record => record.meta.auth)
+
+  console.log(currentUser)
 
   if (currentUser != null) {
     const email = currentUser.email;
@@ -90,10 +97,21 @@ router.beforeEach((to, from, next) => {
 
     if (requireAuth && !adminEntrance) {
       next('/')
-    } else {
+    }
+    else  {
+      next();
+    }
+  }
+  else {
+    if (requireAuth) {
+      next('/')
+    }
+    else {
       next()
     }
   }
+
+
 
 })
 
